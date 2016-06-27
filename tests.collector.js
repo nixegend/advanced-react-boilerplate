d8101a3d56d16b9673c1ab47d18366c758224eb8
Anthony <nixegend@gmail.com>
2016-06-27 23:29:18 +0300

    add modules

--- tests.collector.js
+++
@@ -1,5 +0,0 @@
-// This will search for files ending in .test.js and require them
-// so that they are added to the webpack bundle
-const testsContext = require.context('./src/', true, /.+\.test.js?$/);
-testsContext.keys().forEach(testsContext);
-// module.exports = testsContext;
