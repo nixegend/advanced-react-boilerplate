4aa5e9837b260729c0d9c87c92c26946fff673aa
Anthony <nixegend@gmail.com>
2016-08-06 17:53:56 +0300

    add component Home

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const routes = (
   <Route path='/' component={Base} >
     <IndexRoute component={Home} />
     <IndexRedirect to='/home' />
+    <Route path='home' component={Home} />
     <Route path='about' component={About} />
     <Route path='*' component={NotFound} />
   </Route>
